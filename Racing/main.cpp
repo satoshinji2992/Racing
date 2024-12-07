1f5966970fdf2d75b8a1d728d66cfd665fccdd3c
sato_shinji <wwwshenqi@163.com>
2024-12-07 14:16:13 +0800

    增加nailong

--- Racing/main.cpp
+++ Racing/main.cpp
@@ -11,6 +11,40 @@ const int roadCount = 1884;
 float angle = 0;
 bool turnl = false, turnr = false;
 
+struct point {
+	float x, y, z;
+	float X, Y;
+	float scale, tx, tz;
+	point(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+	void project(int camX, int camY, int camZ, float angle) {
+		tx = (x - camX) * cos(angle) + (z - camZ) * sin(angle);
+		tz = -(x - camX) * sin(angle) + (z - camZ) * cos(angle);
+		if (tz < 0.1f) tz = 0.1f;
+		scale = 1.0f / tz;
+		X = (1 + scale * tx) * WinWidth / 2;
+		Y = (1 - scale * (y - camY)) * WinHeight / 2;
+	}
+};
+
+struct nailong
+{
+	point p[4];
+	nailong(float _x, float _y, float _z) : p{
+		{_x,_y, _z},{_x + 1032,_y,_z},{_x + 1032,_y + 1500,_z},{_x,_y + 1500,_z}} {}
+	bool eat = false;
+};
+
+//把一张长方形图片缩放后绘制在四个点上,用ploygon绘制
+void DrawNailong(RenderWindow& window, Texture& t, nailong n) {
+	ConvexShape polygon(4);
+	polygon.setTexture(&t);
+	polygon.setPoint(0, Vector2f(n.p[0].X, n.p[0].Y));
+	polygon.setPoint(1, Vector2f(n.p[1].X, n.p[1].Y));
+	polygon.setPoint(2, Vector2f(n.p[2].X, n.p[2].Y));
+	polygon.setPoint(3, Vector2f(n.p[3].X, n.p[3].Y));
+	window.draw(polygon);
+}
+
 //道路结构体
 struct Road
 {
@@ -61,11 +95,16 @@ int main()
 	RenderWindow window(VideoMode(WinWidth, WinHeight), "Racing");
 	window.setFramerateLimit(60);
 
-	Texture bg1,bg2;
+	Texture bg1, bg2, nailong,nailong_head;
 	bg1.loadFromFile("cloud.png");
 	bg2.loadFromFile("car.png");
+	nailong.loadFromFile("nailong.png");
+	nailong_head.loadFromFile("nailong_head.png");
 	Sprite s(bg1, IntRect(0, 0, WinWidth, WinHeight));
 	Sprite c(bg2, IntRect(0, 0, WinWidth, WinHeight));
+	Sprite n(nailong, IntRect(0, 0, 533, 500));
+	Sprite n_head(nailong_head, IntRect(0, 0, WinWidth, WinHeight));
+
 	//生成道路
 	std::vector<Road> roads;
 	float x = 0;
@@ -73,15 +112,20 @@ int main()
 	{
 		//共1884,924-,924+
 		float curve;
-		if(i<=123||(456<i&&i<=789)||(1234<i&&i<=1721)) curve = 0.5;
+		if (i <= 123 || (456 < i && i <= 789) || (1234 < i && i <= 1721)) curve = 0.5;
 		else curve = -0.5;
 		x += curve;
 		int y = 0;
 		if (i > 300 && i < 1240)
-			y =1600*sin(i/30.0 - 10);
-        Road r(x*(45), y , (1 + i) * segLength);
+			y = 1600 * sin(i / 30.0 - 10);
+		Road r(x * (45), y, (1 + i) * segLength);
 		roads.push_back(r);
 	}
+
+	//生成nailong
+	struct nailong n1(roads[100].x+rand() % 1500, roads[100].y, roads[100].z);
+	struct nailong n2(roads[450].x+rand() % 1500, roads[450].y, roads[450].z);
+
 	int camZ = 0;
 	int camX = 0;
 	int camY;
@@ -90,6 +134,8 @@ int main()
 	bool isFly = false;
 	int num = 7;
 	int energy = 700;
+	int round = 0;
+	int hit = 0;
 
 	//在画面上显示距离
 	Font font;
@@ -100,9 +146,9 @@ int main()
 	text.setFillColor(Color::Red);
 	text.setPosition(660, 600);
 
-    
+
 	Clock clock;
-	int counter=0;
+	int counter = 0;
 	bool start = false;
 	Text timerText;
 	timerText.setFont(font);
@@ -110,6 +156,8 @@ int main()
 	timerText.setCharacterSize(48);
 	timerText.setFillColor(Color::Cyan);
 	timerText.setPosition(0, 0);
+
+
 
 	//检测键盘输入
 	while (window.isOpen())
@@ -151,11 +199,11 @@ int main()
 					camX += 3 * segLength*sin(-angle);
 					distance += 0.6;
 				}
-				if (Keyboard::isKeyPressed(Keyboard::Space) && Keyboard::isKeyPressed(Keyboard::W)&&energy!=0) {
+				if (Keyboard::isKeyPressed(Keyboard::Space) && Keyboard::isKeyPressed(Keyboard::W)&&energy> 0) {
 					camZ += 2 * segLength * cos(-angle);
 					camX += 2 * segLength * sin(-angle);
 					distance += 0.4;
-					energy -= 1;
+					energy -= 5;
 				}
 			}
 			else {
@@ -164,13 +212,13 @@ int main()
 					camX += segLength * sin(-angle);
 					distance += 0.2;
 					if (Keyboard::isKeyPressed(Keyboard::Space))
-						energy -= 1;
+						energy -= 2;
 				}
 			}
 
 			if (Keyboard::isKeyPressed(Keyboard::S)) {
-				camZ -= segLength * cos(-angle);
-				camX -= segLength * sin(-angle);
+				camZ -= segLength * cos(angle);
+				camX -= segLength * sin(-1*angle);
 				distance -= 0.2;
 			}
 		
@@ -178,8 +226,25 @@ int main()
 				camZ -= 1 * segLength;
 		}
 
+		//当撞到nailong时,能量增加
+		if (!n1.eat && (camZ <n1.p[0].z || camZ >n1.p[0].z ) && camX > n1.p[0].x+400 && camX < n1.p[1].x-400) {
+			n1.eat = true;
+			energy += 100;
+			hit = 30;
+		}
+		if (!n2.eat && (camZ < n2.p[0].z || camZ > n2.p[0].z ) && camX > n2.p[0].x+400 && camX < n2.p[1].x-400) {
+			n2.eat = true;
+			energy += 100;
+			hit = 30;
+		}
+		
 		int totalLength = roadCount * segLength;
-		if (camZ >= totalLength) camZ -= totalLength;
+		if (camZ >= totalLength) {
+			camZ -= totalLength;
+			n1.eat = false;
+			n2.eat = false;
+			round++;
+		}
 		if (camZ < 0) camZ += totalLength;
 
 
@@ -194,6 +259,18 @@ int main()
 
 		window.draw(s);
 		
+
+		//计算nailong的X和Y;
+		n1.p[0].project(camX, camY, camZ - (startPos >= roadCount ? totalLength : 0), angle);
+		n1.p[1].project(camX, camY, camZ - (startPos >= roadCount ? totalLength : 0), angle);
+		n1.p[2].project(camX, camY, camZ - (startPos >= roadCount ? totalLength : 0), angle);
+		n1.p[3].project(camX, camY, camZ - (startPos >= roadCount ? totalLength : 0), angle);
+
+		n2.p[0].project(camX, camY, camZ - (startPos >= roadCount ? totalLength : 0), angle);
+		n2.p[1].project(camX, camY, camZ - (startPos >= roadCount ? totalLength : 0), angle);
+		n2.p[2].project(camX, camY, camZ - (startPos >= roadCount ? totalLength : 0), angle);
+		n2.p[3].project(camX, camY, camZ - (startPos >= roadCount ? totalLength : 0), angle);
+
 		for (int i = startPos; i < startPos + 300;i++) {
 			Road& now = roads[i%roadCount];
             now.project(camX, camY, camZ - (i >= roadCount ? totalLength : 0), angle);
@@ -231,13 +308,35 @@ int main()
 		if (angle > 1) angle = 1;
 		if (angle < -1) angle = -1;
 
+		//绘制nailong
+		DrawNailong(window, nailong, n1);
+		DrawNailong(window, nailong, n2);
+
+		if (hit > 0) window.draw(n_head);
+
+		
+
 		window.draw(c);
 
 		text.setString(std::to_string(distance));
 		window.draw(text);
-		num = (energy+99) / 100;
+		num = (energy + 99) / 100;
 		DrawEnergy(window, num);
 		window.draw(timerText);
+
+		
+
+		//打印round,后面加上"/10"
+		Text roundText;
+		roundText.setFont(font);
+		roundText.setString(std::to_string(round) + "/10");
+		roundText.setCharacterSize(36);
+		roundText.setFillColor(Color::Red);
+		roundText.setPosition(0, 100);
+		window.draw(roundText);
+
+		hit--;
+
 		window.display();
 	}
 
